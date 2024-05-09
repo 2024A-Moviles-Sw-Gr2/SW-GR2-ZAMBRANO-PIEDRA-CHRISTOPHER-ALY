8d3b0ebc7632eb9450b4f41b1127eb13145b3084
Chris <chris.athan12@gmail.com>
2024-05-09 08:49:05 -0500

    clase Kotlin

---
+++ 00-Kotlin/inicio/src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+fun main(){
+    println("Hola mundo")
+    //Inmutables(no se oueden Re Asignar "=")
+    val inmutable: String = "Chris"
+    // inmutable = "Aly" // ERROR
+
+    //Mutable
+    var mutable: String = "Aly"
+    mutable = "Chris" // OK
+    //VAL ES MEJOR QUE VAR
+
+    //Duck Typing
+    var ejemploVariable = "Adrian Eguez"
+    val edadEjemplo: Int = 12
+    val nombre:String = "Adrian"
+    val sueldo:Double = 1.2
+    val estadoCivil: Char = 'C'
+    val mayorEdad: Boolean = true
+    
+}
